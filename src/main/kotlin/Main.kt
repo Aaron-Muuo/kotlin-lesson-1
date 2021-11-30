5d36601a6b06843d9edb8d3863d11828d899aae9
Aaron-Muuo <aaronmuuo60@gmail.com>
2021-11-30 12:17:34 +0300

    Initialized a Kotlin repo

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    // operators
+
+    //mathematical operators
+    var x: Int? //dealing with null values
+    x = 4
+    val y = 20
+    var result = x + y
+    println(result)
+}
